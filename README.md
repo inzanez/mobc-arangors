c835a792549d04457a48ad62e3c00ddf2f5eb034
Gitea <gitea@fake.local>
2020-06-04 13:04:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# mobc-arangors
+
+## Features
+
+By now, the available features of arangors are:
+
+- make connection to arangoDB
+- get list of databases and collections
+- fetch database and collection info
+- create and delete database or collections
+- full featured AQL query
+- support both `async` and sync
---
+++ src/lib.rs
@@ -0,0 +1,57 @@
+use mobc::{Manager, Pool, async_trait};
+use arangors::ClientError;
+
+#[derive(Clone, Debug)]
+pub struct ArangoDBConnectionManager {
+    url: String,
+    username: String,
+    password: String,
+    use_jwt: bool,
+}
+
+impl ArangoDBConnectionManager {
+    pub fn new(
+        url: &str,
+        username: &str,
+        password: &str,
+        use_jwt: bool,
+    ) -> ArangoDBConnectionManager {
+        ArangoDBConnectionManager {
+            url: url.to_owned(),
+            username: username.to_owned(),
+            password: password.to_owned(),
+            use_jwt,
+        }
+    }
+}
+
+#[async_trait]
+impl Manager for ArangoDBConnectionManager {
+    type Connection = arangors::Connection;
+    type Error = ClientError;
+
+   async fn connect(&self) -> Result<Self::Connection, Self::Error> {
+      if self.use_jwt == true {
+          let client = arangors::Connection::establish_jwt(&self.url, &self.username, &self.password).await?;
+          return Ok(client);
+
+      } else {
+          let client = arangors::Connection::establish_basic_auth(&self.url, &self.username, &self.password).await?;
+          return Ok(client);
+      }
+   }
+
+   async fn check(&self, conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
+       conn.validate_server().await?;
+       Ok(conn)
+   }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
