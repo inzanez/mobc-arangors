4006db010808596ff067cf17daec870dc65a82a5
Gitea <gitea@fake.local>
2020-09-04 13:23:25 +0200

    Allow to chose between reqwest and surf, bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-use mobc::{Manager, async_trait};
+use arangors::connection::role::Normal;
 use arangors::ClientError;
+use mobc::{async_trait, Manager};
+
+#[cfg(feature = "reqwest")]
+use arangors::client::reqwest::ReqwestClient;
+#[cfg(feature = "surf")]
+use arangors::client::surf::SurfClient;
 
 #[derive(Clone, Debug)]
 pub struct ArangoDBConnectionManager {
@@ -33,26 +39,46 @@ impl Manager for ArangoDBConnectionManager {
     type Connection = arangors::Connection;
     type Error = ClientError;
 
-   async fn connect(&self) -> Result<Self::Connection, Self::Error> {
-      if self.use_jwt == true {
-          let client = arangors::Connection::establish_jwt(&self.url, &self.username, &self.password).await?;
-          return Ok(client);
+    async fn connect(&self) -> Result<Self::Connection, Self::Error> {
+        if self.use_jwt == true {
+            let client =
+                arangors::Connection::establish_jwt(&self.url, &self.username, &self.password)
+                    .await?;
+            return Ok(client);
+        } else {
+            let client = arangors::Connection::establish_basic_auth(
+                &self.url,
+                &self.username,
+                &self.password,
+            )
+            .await?;
+            return Ok(client);
+        }
+    }
 
-      } else {
-          let client = arangors::Connection::establish_basic_auth(&self.url, &self.username, &self.password).await?;
-          return Ok(client);
-      }
-   }
+    #[cfg(feature = "surf")]
+    async fn check(&self, conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
+        if self.validate {
+            arangors::connection::GenericConnection::<SurfClient, Normal>::validate_server(
+                &self.url,
+            )
+            .await?;
+        }
 
-   async fn check(&self, conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
-       if self.validate {
-           conn.validate_server().await?;
-       }
+        Ok(conn)
+    }
+    #[cfg(feature = "reqwest")]
+    async fn check(&self, conn: Self::Connection) -> Result<Self::Connection, Self::Error> {
+        if self.validate {
+            arangors::connection::GenericConnection::<ReqwestClient, Normal>::validate_server(
+                &self.url,
+            )
+            .await?;
+        }
 
-       Ok(conn)
-   }
+        Ok(conn)
+    }
 }
-
 
 #[cfg(test)]
 mod tests {
